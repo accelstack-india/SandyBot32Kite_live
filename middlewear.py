55cd07b54dc8fed336415d41c3db5627a2b1bfdd
Roop Raj <rooprajt@gmail.com>
2023-02-21 22:48:11 +0530

    Added middleware and subprocess

--- middlewear.py
+++ middlewear.py
@@ -4,15 +4,17 @@ import json
 
 from userController import userController
 
+# receive the stock list in arguments from algorith to execute the stock
 arg_str = sys.argv[1]
 stocks_list = json.loads(arg_str)
 
+# for each stock received in the arguments get the users subscribed to the stock type and send for order execution
 for stocks in stocks_list:
     users_list = userController.get_OptionalTradeUsers("Index " + stocks["type"])
     for user in users_list:
         users_data = json.dumps(user)
         stocks_data = json.dumps(stocks)
         arg_str = f'{users_data}|{stocks_data}'
-        print("------------middlewear subprocess received stock and calling another subprocess to execute the stock for user----------------------")
-        subprocess.Popen(['python', "Options_OrderExecution.py", arg_str])
+        # send the stock details and user details to place the order
+        subprocess.Popen(['python', stocks["type"]+"_OrderExecution.py", arg_str])
 
